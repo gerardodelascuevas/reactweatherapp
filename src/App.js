76edb8fb9354e888ae6283ba364e06c2be5afc9b
gerardodelascuevas <gerdlcp@gmail.com>
2022-03-27 11:41:25 -0600

    cambios en footer

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   }
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=4ae2636d8dfbdc3044bede63951a019b`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${REACT_APP_apiKey}`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
